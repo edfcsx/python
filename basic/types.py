e419321cff0778f3e844e4b2f5b0e449d732823b
eduardo cipriano <edfcsx@gmail.com>
2019-08-16 21:38:49 -0300

    organize files

---
+++ basic/types.py
@@ -0,0 +1,14 @@
+print(True) #boolean
+
+print(False) #boolean
+
+print(1.2 + 1) #Float and Int
+
+print('Hello world!') #String
+
+print('You are a ' + 3 * 'cold ' + 'nice!') #Example for repeat values
+
+print([1, 2, 3]) #List
+
+print({'name': 'Eduardo', 'age': 22}) #Dictionary
+
---
+++ basic/variables.py
@@ -0,0 +1,8 @@
+a = 10
+b = 3.2
+
+print(a + b)
+
+a = "I'm ready a string now"
+
+print(a)
